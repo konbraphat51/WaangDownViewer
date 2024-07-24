374b35494d3e6949c6b330a952d16b7510b344f0
konbraphat51 <konbraphat@gmail.com>
2024-07-24 16:41:30 +0900

    show

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+
+        <Viewer :markdown="'hello'" />
+
+    </div>
+</template>
+
+<script>
+export default Vue.defineComponent({
+    name: 'App',
+    components: {
+        "Viewer": Vue.defineAsyncComponent(() => loadModule("Viewer.vue", options))
+    },
+    setup() {
+        //set up i18n
+        const { t } = VueI18n.useI18n()
+        return { t }
+    },
+})
+</script>
